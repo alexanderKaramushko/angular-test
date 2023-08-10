207237a9e01587f9455d02d72126c933162a41bb
alex.karamushko <aleksandrkaramusko@Air-Aleksandr.nordclan>
2023-08-10 11:48:00 +0400

    remove triple-slash directive

--- src/app/core/models/project.model.ts
+++ src/app/core/models/project.model.ts
@@ -1,7 +1,3 @@
-// todo d.ts не виден глобально
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="shared-kernel.d.ts" />
-
 export type Project = {
   id: ProjectId;
   subject: string;
